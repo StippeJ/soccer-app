a2847ace9f135d602a59581bfb1c5ce6c0bebb64
Jan Stippe <janstippe@gmail.com>
2020-11-26 13:56:54 +0100

    refactor: removed dummy comments

--- app/src/main/java/com/example/group04/soccerapp/MainActivity.java
+++ app/src/main/java/com/example/group04/soccerapp/MainActivity.java
@@ -6,11 +6,10 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Dummy Comment
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
