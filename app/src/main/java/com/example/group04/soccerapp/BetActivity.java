9495148e037cbed520a35a10e172e34117b7d1ad
Jan Stippe <janstippe@gmail.com>
2020-12-06 13:09:25 +0100

    feat: Added new fragment and activity

---
+++ app/src/main/java/com/example/group04/soccerapp/BetActivity.java
@@ -0,0 +1,14 @@
+package com.example.group04.soccerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bet);
+    }
+}
